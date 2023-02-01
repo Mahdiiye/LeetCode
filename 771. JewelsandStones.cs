5c41c99e27f6103347c5d91c915e3a5dc48cf244
Mahdiye <64439609+Mahdiiye@users.noreply.github.com>
2023-02-01 15:39:41 +0330

    Add 771 Problem Solution

---
+++ 771. JewelsandStones.cs
@@ -0,0 +1,26 @@
+﻿public class JewelsandStones
+{
+    static public void Main(String[] args)
+    {
+
+        var solution = new JewelsandStones();
+        solution.NumJewelsInStones("aA", "aAAbbbb");
+    }
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        int num = 0;
+        char[] jwls = jewels.ToCharArray();
+        char[] stns = stones.ToCharArray();
+        for (int i = 0; i < jwls.Length; i++)
+        {
+            for (int j = 0; j < stns.Length; j++)
+            {
+                if (jwls[i] == stns[j])
+                {
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+}
