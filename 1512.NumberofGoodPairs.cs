0782b55fc7a252040ddbdd2aa7d1feb5f603ddf5
Mahdiye <64439609+Mahdiiye@users.noreply.github.com>
2023-02-01 15:26:25 +0330

    Add 1512 Solution

---
+++ 1512.NumberofGoodPairs.cs
@@ -0,0 +1,30 @@
+﻿public class NumberofGoodPairs
+{
+    static public void Main(String[] args)
+    {
+
+        var solution = new NumberofGoodPairs();
+        solution.NumIdenticalPairs(new int[] { 1, 2, 3, 1, 1, 3 });
+    }
+
+    public int NumIdenticalPairs(int[] nums)
+    {
+        var goodPairs = new List<(int, int)>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j] && i < j)
+                {
+                    goodPairs.Add((i,j));
+                }
+            }
+        }
+        foreach (var i in goodPairs)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine(goodPairs.Count);
+        return goodPairs.Count;
+    }
+}
