56d8bd82e0f0bed01bbb565175a52fed4d2222d7
Mahdiye <64439609+Mahdiiye@users.noreply.github.com>
2023-02-04 23:22:58 +0330

    Added 1480 Problem's Solution

---
+++ 1480.RunningSumof1dArray.cs
@@ -0,0 +1,14 @@
+﻿public class RunningSumof1dArray
+{
+    public int[] RunningSum(int[] nums)
+    {
+        int sum = 0;
+        int[] runningSum = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum = sum+ nums[i];
+            runningSum[i] = sum;
+        }
+        return runningSum;
+    }
+}
