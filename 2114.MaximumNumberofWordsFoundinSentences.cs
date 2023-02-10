9995ce7fdbb5b959bdbc6b92cff1367076c93e06
Mahdiye <64439609+Mahdiiye@users.noreply.github.com>
2023-02-10 21:40:18 +0330

    Added 2114 Problem's Solution

---
+++ 2114.MaximumNumberofWordsFoundinSentences.cs
@@ -0,0 +1,14 @@
+﻿public class MaximumNumberofWordsFoundinSentences
+{
+    public int MostWordsFound(string[] sentences)
+    {
+        int[] wordsNum = new int[sentences.Length];
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            string[] words = { };
+            words = sentences[i].Split(' ');
+            wordsNum[i] = words.Length; 
+        }
+        return wordsNum.Max();
+    }
+}
